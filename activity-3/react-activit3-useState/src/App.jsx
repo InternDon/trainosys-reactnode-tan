e2fcef887c2b43bfaaa54339240ba6319b7cd3f7
Kiirodom <tan.dondominic@ue.edu.ph>
2024-11-12 21:58:47 +0800

    Second Commit

---
+++ activity-3/react-activit3-useState/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import LightSwitch from './Components/LightSwitch';
+import Counter from './Components/Counter';
+import ColorPicker from './Components/ColorPicker';
+
+
+const App = () => {
+
+  return (
+    <>
+      <LightSwitch />
+      <Counter />
+      <ColorPicker />
+    </>
+  )
+}
+
+export default App
